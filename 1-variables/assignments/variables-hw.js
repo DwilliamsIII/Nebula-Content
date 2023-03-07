b797cae809e9a3bc808c848083835b7baf141525
DwilliamsIII <lastchief33@yahoo.com>
2023-03-07 18:15:43 -0500

    added variables hw answers

---
+++ 1-variables/assignments/variables-hw.js
@@ -0,0 +1,47 @@
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+
+const animal = 'sloth'
+console.log(animal)
+
+const foodType = 'leaves'
+console.log(foodType)
+
+const lifeSpan = 20
+console.log(lifeSpan)
+
+const speed = 0.17
+console.log(speed)
+
+const scientificName = 'Folivora'
+console.log(scientificName)
+
+const isKingdomAnimalia = true
+console.log(isKingdomAnimalia)
+
+ 
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+const make = 'Ford'
+console.log(make)
+
+const model = 'Thunderbird'
+console.log(model)
+
+const year = 1966
+console.log(year)
+
+const isRagTop = false
+console.log(isRagTop)
+
+const engine = 390
+console.log(engine)
+
+const bodyStyle = '2-door'
+console.log(bodyStyle)
+
+
